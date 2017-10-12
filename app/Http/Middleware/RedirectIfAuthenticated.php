3f53d0389c7a2e34e467db0cc2d9301a4acbea00
hafizhfadh <hafizhfadh94@gmail.com>
2017-10-12 17:38:03 +0700

    New Routes

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,11 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-<<<<<<< HEAD
-            return redirect('/home');
-=======
             return redirect('/');
->>>>>>> e8a8e39f290ba5e127282cc8606d470c6296cc4f
         }
 
         return $next($request);
