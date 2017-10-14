2565cfd583451019cc1f38b84513d7c0c03bac98
muhammadrafi017 <rafi160500@gmail.com>
2017-10-14 18:04:22 +0700

    sync dlu gan

--- app/Http/Controllers/DepartureController.php
+++ app/Http/Controllers/DepartureController.php
@@ -13,10 +13,6 @@ class DepartureController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
     public function index()
     {
         return view('departure.index');
